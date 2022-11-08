fab83218d30c9666479c7955078122b81fd8ef16
cmotadev <cmota.dev@gmail.com>
2022-11-08 15:46:40 -0300

    First commit for ArcGIS Hook

---
+++ airflow/providers/arcgis/hooks/arcgis.py
@@ -0,0 +1,87 @@
+from arcgis.gis import GIS
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from typing import Any, Callable, cast
+# from airflow.exceptions import AirflowException
+from airflow.hooks.base import BaseHook
+
+
+class ArcGISHook(BaseHook):
+    """
+    """
+    conn_name_attr = "arcgis_conn_id"
+    default_conn_name = "arcgis_default"
+    conn_type = "arcgis"
+    hook_name = "ArcGIS Online/Enterprise"
+
+    def __init__(self, arcgis_conn_id=default_conn_name):
+        super().__init__()
+        self.arcgis_conn_id=arcgis_conn_id
+        self.portal_url = ''
+    
+    def get_conn(self) -> Any:
+        """
+        """
+        conn_id = getattr(self, cast(str, self.conn_name_attr))
+        kwargs = {}
+
+        if conn_id:
+            conn = self.get_connection(conn_id)            
+
+            if conn.host and "://" in conn.host:
+                self.portal_url = conn.host
+
+            else:
+                schema = conn.schema if conn.schema else "https"
+                host = conn.host if conn.host else ""
+
+                self.portal_url = schema + "://" + host
+            
+            # Parse URL to check and add some completions
+            result = urlparse(self.portal_url)
+            assert all([result.scheme, result.netloc])
+
+            # Add port
+            if conn.port:
+                print("port: %s", conn.port)
+                result = result._replace(netloc=result.netloc + ":" + str(conn.port))
+
+            # Check webadaptor and other stuff
+            if conn.extra:
+                kwargs = conn.extra_dejson
+
+                # Add webadaptor
+                webadaptor = kwargs.get("webadaptor")                
+
+                if webadaptor and result.path in ("", "/"):
+                    result = result._replace(path=webadaptor)
+
+                # remove redundant parameters
+                for key in ['url', 'username', 'password']:
+                    if key in kwargs:
+                        self.log.warn("Ignoring key '%s', passed as 'Extra' field", key)
+                        del kwargs[key]
+            
+            # dump portal URL
+            self.portal_url = result.geturl()
+
+        if self.portal_url:
+            kwargs.update({'url': self.portal_url})
+        
+        return GIS(username=conn.login, password=conn.get_password(), **kwargs)
+
+
+    def run(self):
+        gis = self.get_conn()
+        return gis
+
+    def test_connection(self):
+        """Test HTTP Connection"""
+        try:
+            self.run()
+            return True, f"Connected to ArcGIS Portal/Enterprise: {self.portal_url}"
+
+        except Exception as e:
+            soup = BeautifulSoup(str(e), 'html.parser')
+
+            return False, f"Failed to check URL {self.portal_url}: [{e.__class__.__name__}]: {soup.get_text()}"
---
+++ setup.py
@@ -0,0 +1,30 @@
+import io
+from os.path import dirname, join
+from setuptools import find_packages, setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf-8")
+    ).read()
+
+
+setup(
+    name="apache-airflow-providers-arcgis",
+    version="1.0.0",
+    description="ArcGIS Online/ArcGIS Enterprise Apache Airflow Provider",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Carlos Eduardo Mota",
+    author_email="carlos.mota@sgb.gov.br",
+    url="https://github.com/nds-cprm/airflow-arcgis-enterprise-provider",
+    license="GPL",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "apache-airflow>=2.4.0",
+        "arcgis==2.0.*",
+        "beautifulsoup4==4.*"
+    ],
+)
